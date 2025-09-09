7f9ad67b898c95626dfb3dd0218755f258f268d1
maddalenapag <maddalena.pagliarulo@gmail.com>
2025-09-09 22:08:03 +0200

    Aggiunto script di popolamento database
    Inseriti dati di esempio per passeggeri, stazioni, tratte, biglietti, pagamenti e fermate intermedie.

--- popola_dati.sql
+++ popola_dati.sql
@@ -45,10 +45,11 @@ INSERT INTO Pagamento (biglietto_id, metodo_pagamento, data_pagamento, esito) VA
 (1, 'Carta di credito', '2025-07-01', 'OK'),
 (2, 'PayPal', '2025-07-02', 'OK'),
 (3, 'Bonifico', '2025-07-03', 'OK'), 
-(4, ' Carta di credito ', '2025-07-04', 'KO'), -- Pagamento non riuscito
+(4, 'Carta di credito ', '2025-07-04', 'KO'), -- Pagamento non riuscito
 (5, 'PayPal', '2025-07-05', 'OK');
 
 INSERT INTO FermataIntermedia (tratta_id, stazione_id, ordine) VALUES
  (1, 6, 1),  -- Milano → Roma con fermata a Firenze
 (2, 7, 1),  -- Venezia → Napoli con fermata a Bologna
-(3, 5, 1);  -- Torino → Napoli con fermata a Roma
+(3, 5, 1);  -- Napoli → Torino con fermata a Roma
+
